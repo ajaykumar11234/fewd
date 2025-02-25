b71ea71088fb6997b320d83e787337bf90c8b40c
ajay kumar <ajaykumarmekala42@gmail.com>
2025-02-25 10:36:48 +0530

    commit

---
+++ reactapp/myreact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Ajay from "./components/Ajay"
+
+function App() {
+  return (
+    <div className="App">
+      <Ajay/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/myreact/src/components/Ajay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ajay = () => {
+  return (
+    <div>
+      <h3>This is Ajay Kumar</h3>
+      <h2>I am from KMIT</h2>
+    </div>
+  )
+}
+
+export default Ajay
